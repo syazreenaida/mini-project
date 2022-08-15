72b256d3b8cbecf50a540ff83723be1094eece96
syazreenaida <syazreenaida@gmail.com>
2022-08-15 15:03:58 +0800

    firsr commit

---
+++ src/main/java/miniproject/com/miniprojectsyaz/MiniprojectSyazApplication.java
@@ -0,0 +1,13 @@
+package miniproject.com.miniprojectsyaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojectSyazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojectSyazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miniproject/com/miniprojectsyaz/MiniprojectSyazApplicationTests.java
@@ -0,0 +1,13 @@
+package miniproject.com.miniprojectsyaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojectSyazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
